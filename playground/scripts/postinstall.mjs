a7c750bf5b92c357586c50af2b71bab4a58f443e
Leo Yang <qa894178522@qq.com>
2025-10-15 12:56:46 +0800

    chore(play): update scripts

--- playground/scripts/postinstall.mjs
+++ playground/scripts/postinstall.mjs
@@ -4,5 +4,5 @@ import process from 'node:process'
 import { checkManifestJsonFileSync } from '@uni-aide/vite-plugin-manifest'
 import { checkPagesJsonFileSync } from '@uni-aide/vite-plugin-pages'
 
-checkPagesJsonFileSync(path.join(process.cwd(), 'src'))
-checkManifestJsonFileSync(path.join(process.cwd(), 'src'))
+checkPagesJsonFileSync(path.join(process.cwd(), 'src', 'pages.json'))
+checkManifestJsonFileSync(path.join(process.cwd(), 'src', 'manifest.json'))
