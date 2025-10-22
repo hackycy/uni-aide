3699c53937994ebb4e9b6f71a236e28ba2240ac1
Leo Yang <qa894178522@qq.com>
2025-10-22 21:03:53 +0800

    wip(core): ast

---
+++ packages/core/src/ast.ts
@@ -0,0 +1,9 @@
+import type * as t from '@babel/types'
+
+/**
+ * 查找注释所属的节点
+ */
+export function findCommentBelongsToNode(_comment: t.Comment, _ast: t.File): t.Node | null {
+  // TODO
+  return null
+}
