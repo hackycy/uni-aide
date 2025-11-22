eb067df9c6659032fe77e96ffa0fffe0ca3a8699
Yuan <qa894178522@qq.com>
2025-11-23 01:47:33 +0800

    docs: ignore script error

--- playground/scripts/postinstall.mjs
+++ playground/scripts/postinstall.mjs
@@ -4,5 +4,10 @@ import process from 'node:process'
 import { checkManifestJsonFileSync } from '@uni-aide/unplugin-uni-manifest'
 import { checkPagesJsonFileSync } from '@uni-aide/unplugin-uni-pages'
 
-checkPagesJsonFileSync(path.join(process.cwd(), 'src', 'pages.json'))
-checkManifestJsonFileSync(path.join(process.cwd(), 'src', 'manifest.json'))
+try {
+  checkPagesJsonFileSync(path.join(process.cwd(), 'src', 'pages.json'))
+  checkManifestJsonFileSync(path.join(process.cwd(), 'src', 'manifest.json'))
+}
+catch {
+  // ignore
+}
