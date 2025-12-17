da775222a232aae315d9c96346a17b55befaa952
Changyuan Yang <26972260+hackycy@users.noreply.github.com>
2025-12-18 00:35:19 +0800

    docs(playground): update demo

---
+++ playground/src/pages-sub/internal/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>Pages Sub Internal</view>
+</template>
+
+<script setup lang="ts">
+import { onShow } from "@dcloudio/uni-app";
+
+onShow(() => {
+  console.log("onShow");
+});
+</script>
+
+<route lang="jsonc" part="subPackage">
+{
+  "style": {
+    "navigationBarTitleText": "Pages Sub Internal",
+    // #ifdef H5
+    "navigationStyle": "custom"
+    // #endif
+  }
+}
+</route>
